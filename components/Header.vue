221ab483a866e0150a42a166d4870b2bf4ab9ba7
Don <donshius@inbox.lt>
2020-07-11 12:03:48 +0100

    add custom css, cleanup README files

---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="header">
+        <div class="container">
+            Inner
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.header {
+    @apply bg-white;
+    @apply border-b;
+}
+</style>
