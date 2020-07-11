67e5a8a3b15419c1a50890ce0b9e1b0000057701
Don <donshius@inbox.lt>
2020-07-11 13:07:20 +0100

    add user profile button to header controls, add some default stock profile photos

--- components/Header.vue
+++ components/Header.vue
@@ -8,7 +8,7 @@
                 <div class="search-bar">
                     <input type="text" placeholder="Search" class="border text-center rounded" />
                 </div>
-                <div class="header-controls flex items-center">
+                <div class="header-controls flex">
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.672 11H17v6c0 .445-.194 1-1 1h-4v-6H8v6H4c-.806 0-1-.555-1-1v-6H1.328c-.598 0-.47-.324-.06-.748L9.292 2.22c.195-.202.451-.302.708-.312.257.01.513.109.708.312l8.023 8.031c.411.425.539.749-.059.749z"/></svg>
                     </div>
@@ -20,6 +20,9 @@
                     </div>
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17.19 4.156c-1.672-1.535-4.383-1.535-6.055 0L10 5.197 8.864 4.156c-1.672-1.535-4.382-1.535-6.054 0-1.881 1.726-1.881 4.519 0 6.245L10 17l7.19-6.599c1.88-1.726 1.88-4.52 0-6.245zm-1.066 5.219L10 15.09 3.875 9.375c-.617-.567-.856-1.307-.856-2.094s.138-1.433.756-1.999c.545-.501 1.278-.777 2.063-.777.784 0 1.517.476 2.062.978L10 7.308l2.099-1.826c.546-.502 1.278-.978 2.063-.978s1.518.276 2.063.777c.618.566.755 1.212.755 1.999s-.238 1.528-.856 2.095z"/></svg>
+                    </div>
+                    <div>
+                        <img class="rounded-full border" src="/images/profile-0.png">
                     </div>
                 </div>
             </div>
@@ -55,5 +58,10 @@
     width: 28px;
     height: 28px;
     fill: currentColor;
+}
+
+.header-controls > div > img {
+    width: 24px;
+    height: 24px;
 }
 </style>
